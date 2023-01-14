100a37cf4f3f49e1b54fe36d80e216adc5ae8964
Carlos Zapata <carloszapata@Carloss-MBP.cable.rcn.com>
2023-01-14 12:30:52 -0600

    first         commit

---
+++ backend/app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ffd600bd2191bd25f1f08ae48aa34448'),
+    },
+  },
+});
